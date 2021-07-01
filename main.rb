a33e7344d89697e35c6a188d747b8d5c3fc29eac
Fran <78037877+franlocus@users.noreply.github.com>
2021-07-01 19:06:57 -0300

    Added preview return

--- main.rb
+++ main.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# knight_moves([0,0],[1,2]) == [[0,0],[1,2]]
-# knight_moves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
-# knight_moves([3,3],[0,0]) == [[3,3],[1,2],[0,0]]
-
 require_relative 'knight'
 require_relative 'board'
    
@@ -14,3 +10,21 @@ puts board.knight_moves([0,0],[3,3])
 puts board.knight_moves([0,0],[1,2])
 
 puts board.knight_moves([0,0],[7,7])
+
+# You made it in 2 moves!  Here's your path:
+# [0, 0]
+# [1, 2]
+# [3, 3]
+
+# You made it in 1 moves!  Here's your path:
+# [0, 0]
+# [1, 2]
+
+# You made it in 6 moves!  Here's your path:
+# [0, 0]
+# [1, 2]
+# [2, 4]
+# [3, 6]
+# [5, 7]
+# [6, 5]
+# [7, 7]
